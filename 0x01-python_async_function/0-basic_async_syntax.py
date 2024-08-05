f12906ad407ce9aabc3e4bd9d8bb9d04c65c684d
eddcode123 <edwinmutugi29@gmail.com>
2024-08-05 13:10:38 +0300

    adding async coroutine to delay return

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """ The function uses async and random to
+    delay the function return by random seconds
+    then returning the seconds it delayed
+    """
+
+    delay_time = random.uniform(0, max_delay)
+    await asyncio.sleep(delay_time)
+    return delay_time
