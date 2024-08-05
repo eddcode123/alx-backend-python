b0fd53dad851ce74866d5a89274f0546ad774d32
eddcode123 <edwinmutugi29@gmail.com>
2024-08-05 15:48:34 +0300

    same as wait_n only changed the name of the function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -16,5 +16,5 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     the delayed seconds from each
     wait_delay call
     """
-    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+    tasks = [task_wait_random(max_delay)for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
