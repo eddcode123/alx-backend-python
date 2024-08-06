d80db33ae15f5f2eb865d8fc34bf2ec582856ad1
eddcode123 <edwinmutugi29@gmail.com>
2024-08-06 13:32:58 +0300

    coroutine that yeilds a random number and waits 1 second in its 10 iteration

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ This coroutine loops 10 times, waiting 1 second
     after each iteration, then yields a random float
     between 0 and 10.
