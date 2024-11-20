3ba23b5a1f90906dafcb263990f0586c59c494fb
eddcode123 <edwinmutugi29@gmail.com>
2024-11-20 17:21:12 +0300

    type-annotated function to_kv that takes a string k and an int OR float v as arguments and returns a tuple. The first element of the tuple is the string k. The second element is the square of the int/float v

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Takes a sting and float or int creates a tuple."""
+"""Takes a string and float or int creates a tuple."""
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Create and return a tuple with the first value being the string
     followed by a float or int."""
-    return (k, v)
+    return k, v ** 2
