5a73a4808ae31b5375288d90e762feb628d87d3c
eddcode123 <edwinmutugi29@gmail.com>
2024-11-20 17:50:35 +0300

    type-annotated function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,19 +4,15 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """
-    Returns a function that multiplies a given value by the specified multiplier.
-
+    """Multiplies a given value by the specified multiplier.
     Args:
         multiplier (float): The value to multiply any input by.
-
     Returns:
-        Callable[[float], float]: A function that takes a float and returns the product 
-                                  of the input value and the multiplier.
+        A function that multiplies a float by multiplier.
     """
-    
+
     def function_multiplier(value: float) -> float:
         """Multiplies a float by multiplier"""
         return multiplier * value
-    
+
     return function_multiplier
