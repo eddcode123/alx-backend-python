3e3f79b303d1e29ddcddaeadc5c3fbf49da88c3f
eddcode123 <edwinmutugi29@gmail.com>
2024-12-13 17:17:07 +0300

    mock requests.get

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,9 @@
 """Test access_nested_map"""
 
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,6 +26,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """Raise keyerror exception in the parameterized case"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test case for the get_json function"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, expected, mock_get):
+        """Test that get_json retrieves and returns JSON data correctly"""
+        # Configure the mock
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = expected
+
+        # call the the function get_json
+        result = get_json(test_url)
+
+        self.assertEqual(result, expected)
+        mock_get.assert_called_once_with(test_url)
 
 
 if __name__ == '__main__':
